39cc3f714c8b075d22401111ba4f202df4753425
pmalviya17 <pritesh.malviya@cognizant.com>
2019-01-16 01:45:27 +0530

    Adding application configurations
    Adding application configurations such as use of custom errors on entity fields, use of password encoder services

---
+++ src/main/java/com/uxpsystems/assignment/config/CustomLogMsgConfiguration.java
@@ -0,0 +1,16 @@
+package com.uxpsystems.assignment.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+public class CustomLogMsgConfiguration {
+    @Bean(name = "messageSource")
+    public ReloadableResourceBundleMessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageBundle = new ReloadableResourceBundleMessageSource();
+        messageBundle.setBasename("classpath:messages/messages");
+        messageBundle.setDefaultEncoding("UTF-8");
+        return messageBundle;
+    }
+}
